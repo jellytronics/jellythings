a1c4ee9a94b3af14e05a19620cf038b9b1c32078
Jeremias Wong <jellyjellyrobot@gmail.com>
2014-01-23 10:18:29 +0800

    added \ to spaces

--- Experiments/LCD Controller/install.sh
+++ Experiments/LCD Controller/install.sh
@@ -11,7 +11,7 @@ git clone https://github.com/quick2wire/quick2wire-python-api.git
 
 echo "Setting Enviornment Variables"
 
-export QUICK2WIRE_API_HOME=~/jellythings/Experiments/LCD Controller/quick2wire-python-api
+export QUICK2WIRE_API_HOME=~/jellythings/Experiments/LCD\ Controller/quick2wire-python-api
 export PYTHONPATH=$PYTHONPATH:$QUICK2WIRE_API_HOME
 
 echo "Cloning i2clibraries"
