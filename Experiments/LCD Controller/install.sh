8daace1a23f8ed9e852c0d5d0d15c8347097d186
Jeremias Wong <jellyjellyrobot@gmail.com>
2014-01-23 09:47:41 +0800

    added echoes

--- Experiments/LCD Controller/install.sh
+++ Experiments/LCD Controller/install.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
 
+echo "Installing python3"
+
 sudo apt-get install python3
 
+echo "Cloning quick2wire"
+
 git clone https://github.com/quick2wire/quick2wire-python-api.git
+
+echo "Setting Enviornment Variables"
+
 export QUICK2WIRE_API_HOME=~/jellythings/Experiments/LCD Controller/quick2wire-python-api
 export PYTHONPATH=$PYTHONPATH:$QUICK2WIRE_API_HOME
+
+echo "Cloning i2clibraries"
 
 git clone https://bitbucket.org/thinkbowl/i2clibraries.git
--- Experiments/LCD Controller/startup.sh
+++ Experiments/LCD Controller/startup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Running Startup Script"
+
 export QUICK2WIRE_API_HOME=~/jellythings/Experiments/LCD Controller/quick2wire-python-api
 export PYTHONPATH=$PYTHONPATH:$QUICK2WIRE_API_HOME
 
