4f53f32a7f8871ff5f37f6be56a32bd3dbbc64b0
Jeremias Wong <jellyjellyrobot@gmail.com>
2014-01-23 09:24:19 +0800

    one times python file for consumption

---
+++ Experiments/LCD Controller/try1.py
@@ -0,0 +1,15 @@
+from i2clibraries import i2c_lcd
+from time import * 
+
+# Configuration parameters
+# I2C Address, Port, Enable pin, RW pin, RS pin, Data 4 pin, Data 5 pin, Data 6 pin, Data 7 pin, Backlight pin (optional)
+lcd = i2c_lcd.i2c_lcd(0x27,0, 2, 1, 0, 4, 5, 6, 7, 3)
+
+# If you want to disable the cursor, uncomment the following line
+# lcd.command(lcd.CMD_Display_Control | lcd.OPT_Enable_Display)
+
+lcd.backLightOn()
+
+lcd.writeString("Python I2C LCD")
+lcd.setPosition(2, 3) 
+lcd.writeString("For the Pi")
