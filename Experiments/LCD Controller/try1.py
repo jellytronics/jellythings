1c0996089e1fe433945fbea955103331a4917a21
Jeremias Wong <wong.jeremias@gmail.com>
2014-01-25 20:20:02 +0000

    Installed python-quickwire api

--- Experiments/LCD Controller/try1.py
+++ Experiments/LCD Controller/try1.py
@@ -3,7 +3,7 @@ from time import *
 
 # Configuration parameters
 # I2C Address, Port, Enable pin, RW pin, RS pin, Data 4 pin, Data 5 pin, Data 6 pin, Data 7 pin, Backlight pin (optional)
-lcd = i2c_lcd.i2c_lcd(0x27,0, 2, 1, 0, 4, 5, 6, 7, 3)
+lcd = i2c_lcd.i2c_lcd(0x20,0, 2, 1, 0, 4, 5, 6, 7, 3)
 
 # If you want to disable the cursor, uncomment the following line
 # lcd.command(lcd.CMD_Display_Control | lcd.OPT_Enable_Display)
