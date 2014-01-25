cce611da60d962e30c47c043647e57dd739d3ded
Jeremias Wong <jellyjellyrobot@gmail.com>
2014-01-26 04:50:48 +0800

    Increased functionality on all install scripts, added some sense into am_alive ttytter shell script. startup scripts given a boost.
    Very buggy still, will work the kinks out in due time.

--- Experiments/LCD Controller/install.sh
+++ Experiments/LCD Controller/install.sh
@@ -3,17 +3,31 @@
 
 echo "Installing python3"
 
-sudo apt-get install python3
+sudo apt-get install python3 python-virtualenv python-pip python3-setuptools
 
-echo "Cloning quick2wire"
+#echo "Cloning quick2wire"
 
-git clone https://github.com/quick2wire/quick2wire-python-api.git
+#git clone https://github.com/quick2wire/quick2wire-python-api.git
 
 echo "Setting Enviornment Variables"
 
 export QUICK2WIRE_API_HOME=~/jellythings/Experiments/LCD\ Controller/quick2wire-python-api
 export PYTHONPATH=$PYTHONPATH:$QUICK2WIRE_API_HOME
 
-echo "Cloning i2clibraries"
+#echo "Cloning i2clibraries"
 
-git clone https://bitbucket.org/thinkbowl/i2clibraries.git
+#git clone https://bitbucket.org/thinkbowl/i2clibraries.git
+
+echo "Setting Python Enviornment - Quick2wire"
+
+#https://github.com/quick2wire/quick2wire-python-api
+cd ~/jellythings/Experiments/LCD\ Controller/quick2wire-python-api
+python3 ~/jellythings/Experiments/LCD\ Controller/quick2wire-python-api/setup.py install
+
+#https://github.com/quick2wire/quick2wire-gpio-admin
+cd ~/jellythings/Experiments/LCD\ Controller/quick2wire-gpio-admin
+make
+sudo make install
+
+#to uninstall, just uncomment the bottom
+#sudo make uninstall
