80f90856a6bcfa0a6a8c89d25517e6d069f1c116
Jeremias Wong <jellyjellyrobot@gmail.com>
2014-02-14 11:32:59 +0800

    cleaning up

--- cs1020hw/Labs/Lab3a-SI/SessionA/A/skeleton/LongestSubstring.java
+++ cs1020hw/Labs/Lab3a-SI/SessionA/A/skeleton/LongestSubstring.java
@@ -17,10 +17,43 @@ public class LongestSubstring {
 
 		String input = sc.next();
 		sc.nextLine();
+		String[] array = input.split("");
 
+		//Debug
 		System.out.println(input);
-		System.out.println(input.length());
+		//System.out.println(input.length());
+		//System.out.println(array);
+		System.out.println(array.length);
 
+		printLongestSubstring(array);
 
 	}
+
+	public static void printLongestSubstring(String[] array){
+
+		int length = 0;
+
+		for (int i = 1; i < array.length; i++){
+			for (int j = i + 1; j < array.length - i; j++){
+				if (array[j].equalsIgnoreCase(array[i])){
+					break;
+				}
+				for (int k = array.length - j; k > j; k--){
+					if ((k - j) > length){
+						length = k - j;
+					}
+					if (array[j].equalsIgnoreCase(array[k])){
+						i = j;
+						j = array.length;
+						break;
+					}
+				}
+			}
+		}
+
+		System.out.println(length);
+
+	}
+
+
 }
